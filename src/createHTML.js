3dae28d0bf8709faf012001801573f41f135c52b
Tyler Hudson <tylerhudson96@gmail.com>
2022-09-13 20:18:53 -0400

    styling and HTML formatting with bootstrap

--- src/createHTML.js
+++ src/createHTML.js
@@ -1,12 +1,12 @@
 const createManager = function (manager) {
     return `
-    <div class="">
-        <div class="">
-            <div class="card-header">
+    <div class="col-4 mt-4">
+        <div class="card h-100">
+            <div class="cardHeader">
                 <h3>${manager.name}</h3>
-                <h4>Manager</h4>
+                <h4>🚀 Manager</h4>
             </div>
-            <div class="card-body">
+            <div class="cardBody">
                 <p class="id">ID: ${manager.id}</p>
                 <p class="email">Email: <a href="mailto:${manager.email}">${manager.email}</a></p>
                 <p class="office">Office Number: ${manager.officeNumber}</p>
@@ -18,13 +18,13 @@ const createManager = function (manager) {
 
 const createEngineer = function (engineer) {
     return `
-    <div class="">
-        <div class="">
-            <div class="card-header">
+    <div class="col-4 mt-4">
+        <div class="card h-100">
+            <div class="cardHeader">
                 <h3>${engineer.name}</h3>
-                <h4>Engineer</h4>
+                <h4>💻 Engineer</h4>
             </div>
-            <div class="card-body">
+            <div class="cardBody">
                 <p class="id">ID: ${engineer.id}</p>
                 <p class="email">Email: <a href="mailto:${engineer.email}">${engineer.email}</a></p>
                 <p class="github">Github: <a href="https://github.com/${engineer.github}">${engineer.github}</a></p>
@@ -36,13 +36,13 @@ const createEngineer = function (engineer) {
  
 const createIntern = function (intern) {
     return `
-    <div class="">
-        <div class="">
-            <div class="card-header">
+    <div class="col-4 mt-4">
+        <div class="card h-100">
+            <div class="cardHeader">
                 <h3>${intern.name}</h3>
-                <h4>Intern</h4>
+                <h4>📚 Intern</h4>
             </div>
-            <div class="card-body">
+            <div class="cardBody">
                 <p class="id">ID: ${intern.id}</p>
                 <p class="email">Email:<a href="mailto:${intern.email}">${intern.email}</a></p>
                 <p class="school">School: ${intern.school}</p>
@@ -94,22 +94,25 @@ const generateTeamPage = function (employeeCards) {
       <meta charset="UTF-8">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
       <title>Team Profile</title>
+      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+      <link href="https://fonts.googleapis.com/css2?family=JetBrains+Mono:wght@300&family=Ubuntu&display=swap" rel="stylesheet">
+      <link rel="stylesheet" href="./style.css">
   </head>
   <body>
       <header>
-          <nav class="">
-              <span>Team Profile</span>
+          <nav class="mb-0 h1 w-100 text-center" id="navBar">
+              <span>Team Overview</span>
           </nav>
       </header>
       <main>
-          <div class="">
-              <div class="" id="team-cards">
+          <div class="container">
+              <div class="row justify-content-center">
                   <!--Team Cards-->
                   ${employeeCards}
               </div>
           </div>
       </main>
-      
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>    
   </body>
   </html>
 `;
